ad0a1c1c762f7842e80cc2f0b1fca5c97bf4b28a
sergijluk <Sergii.Lyuk@externals.adidas.com>
2026-02-28 08:32:42 +0200

    L21 first commit

---
+++ L21/src/test/java/com/edu/l21/L21ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edu.l21;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class L21ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
