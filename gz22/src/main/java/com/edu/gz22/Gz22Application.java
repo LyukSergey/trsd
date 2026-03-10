87344528b49b195bbe07e9cec09bfbb8d2229cbc
sergijluk <Sergii.Lyuk@externals.adidas.com>
2026-03-10 11:40:11 +0200

    gz22

---
+++ gz22/src/main/java/com/edu/gz22/Gz22Application.java
@@ -0,0 +1,13 @@
+package com.edu.gz22;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Gz22Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Gz22Application.class, args);
+    }
+
+}
---
+++ gz22/src/test/java/com/edu/gz22/Gz22ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edu.gz22;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Gz22ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
